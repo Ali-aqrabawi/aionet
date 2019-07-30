be586877f84feb93e252f651647ad1916269bc10
ali <aaqrabaw@mobilenoc.mobi>
2019-07-30 14:14:48 +0300

    version inc

--- aionet/version.py
+++ aionet/version.py
@@ -2,7 +2,7 @@
 aionet Version information
 """
 
-__version__ = "0.0.42"
+__version__ = "0.0.43"
 __author__ = "ali aqrabawi"
 __author_email__ = "aaqrabaw@gmail.com"
 __url__ = "http://aionet.readthedocs.io/"
